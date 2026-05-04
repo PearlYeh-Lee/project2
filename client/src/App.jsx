b2938db1e87c5d22d0d15d2c532eb7540763124a
PearlYeh-Lee <pearlkailing@gmail.com>
2026-05-04 11:19:41 -0700

    Made input box that on submit, console.log's what was in each input box. Then, I tried to render it. I'm going to look at mapping lists to get it to show up. Next, back end.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,120 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios' // Make sure you've run: npm install axios
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+function Submit({newName, newAge, newBreed, name, age, breed, clickInputHandler}){
+  function newNameSetter (event){
+    newName(event.target.value)
+  }
+  function newAgeSetter (event){
+    newAge(event.target.value)
+  }
+  function newBreedSetter (event){
+    newBreed(event.target.value)
+  }
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+    <input onChange={newNameSetter}type="text" defaultValue={name}/>
+    <input onChange={newAgeSetter} type="text" defaultValue={age}/>
+    <input onChange={newBreedSetter} type="text" defaultValue={breed}/>
+    <button onClick={clickInputHandler}> Submit </button>
+    </>
+    )
+  }
+function Display(name){
+  return(
+    <>
+    <div> {name} </div>
+    </>
+  )
+}
+function App() {
+  const [inputName, setInputName]=useState("Name")
+  const [inputAge, setInputAge]=useState("Age")
+  const [inputBreed, setInputBreed]=useState("Breed")
+  function onClickSubmit(){
+    console.log(inputName, inputAge, inputBreed)
+  }
+  function newNameListener (value){
+    setInputName(value)
+  }
+  function newAgeListener (value){
+    setInputAge(value)
+  }
+  function newBreedListener (value){
+    setInputBreed(value)
+  }
+  return (
+    <>
+    <Submit newName={newNameListener} newAge={newAgeListener} newBreed={newBreedListener} name={inputName} age={inputAge} breed={inputBreed} clickInputHandler={()=> onClickSubmit()}/>
+    <Display name={inputName}></Display>
     </>
   )
 }
